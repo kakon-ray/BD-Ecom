58d9948d8054b687ebc86a445e66b920820f1b91
Kakon Ray <thisiskakonray@gmail.com>
2022-03-22 21:35:05 +0600

    first commit

---
+++ src/component/Header.js/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <a href="/">BD Ecom</a>
+      <ul className="ms-auto">
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
