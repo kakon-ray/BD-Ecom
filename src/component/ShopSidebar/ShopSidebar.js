2f92aee9d9f9a3db9940eaab044b66ce3f09e883
Kakon Ray <thisiskakonray@gmail.com>
2022-03-23 19:33:02 +0600

    stiky navbar add

--- src/component/ShopSidebar/ShopSidebar.js
+++ src/component/ShopSidebar/ShopSidebar.js
@@ -5,15 +5,30 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const ShopSidebar = ({ cart }) => {
+  //   console.log(cart);
+
+  // array reduce mathod usegin totalprice in the props cart
+  const totalPrice = cart.reduce((total, current) => {
+    return total + current.price;
+  }, 0);
+
+  const totalShipingCarj = cart.reduce((total, current) => {
+    return total + current.shipping;
+  }, 0);
+
+  const totalText = (totalPrice * 10) / 100;
+
+  const grandTotal = totalPrice + totalShipingCarj + totalText;
+
   return (
-    <div>
+    <div className="cart-siderbar-design">
       <h1 className="Order">Order Summery</h1>
       <div className="order-body">
         <h3>Selected Items: {cart.length}</h3>
-        <h3>Total Price:: {cart.length}</h3>
-        <h3>Total Shipping Charge: {cart.length}</h3>
-        <h3>Tax: {cart.length}</h3>
-        <h3>Grand Total: {cart.length}</h3>
+        <h3>Total Price: ${totalPrice}</h3>
+        <h3>Total Shipping Charge: ${totalShipingCarj}</h3>
+        <h3>Tax: ${totalText}</h3>
+        <h3>Grand Total: ${grandTotal}</h3>
       </div>
       <div>
         <button className="clear-cart">
